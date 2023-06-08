8e3d9544afed327b859ed987f3f2ff5f3bfe82c5
orymikoto <rosbergroll99@gmail.com>
2023-06-08 16:30:00 +0700

    migration update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\KeranjangPenggunaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/keranjang', [KeranjangPenggunaController::class, 'index'])->name('cart.index');
+Route::post('/keranjang/increment/{id_keranjang}', [KeranjangPenggunaController::class, 'incrementQuantity'])->name('cart.increment');
+Route::post('/keranjang/decrement/{id_keranjang}', [KeranjangPenggunaController::class, 'decrementQuantity'])->name('cart.decrement');
